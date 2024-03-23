158d0066e11fca2a8884ecd20d948bcf1dbb4389
leesoul2 <159003227+leesoul2@users.noreply.github.com>
2024-03-24 07:47:36 +0900

    입양 페이지 컨트롤러

--- KHSemiProject/src/main/java/kh/mclass/semi/project/controller/AdoptMainController.java
+++ KHSemiProject/src/main/java/kh/mclass/semi/project/controller/AdoptMainController.java
@@ -27,7 +27,7 @@ public class AdoptMainController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/WEB-INF/views/adoptMain.jsp").forward(request, response);
 	}
 
 	/**
